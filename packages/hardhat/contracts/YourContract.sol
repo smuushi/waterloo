24ad7008425e1f752a467d2004e0a25c3982a852
Kelvin Yeung <kelvinyeung@Kelvins-MacBook-Air.local>
2023-06-24 22:43:36 -0400

    work

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -91,14 +91,14 @@ contract YourContract {
     struct Villager {
         string name;
         uint dna;
-
         address owner;
-
+        uint[] inventory;
     }
 
     
 
     Villager[] public allVillagers;
+    NFT[] public allNFTs;
 
     function showAllVillagers() public view returns(Villager[] memory) {
         return allVillagers;
@@ -128,7 +128,8 @@ contract YourContract {
     mapping (address => uint) ownerVillagerCount; // hash for wallet address => their number of villagers they own. 
 
     function _createVillager(string memory _name, uint _dna) private{
-        allVillagers.push(Villager(_name, _dna, msg.sender));
+        // NFT[] memory emptyInventory = new NFT[](10);
+        allVillagers.push(Villager(_name, _dna, msg.sender, new uint[](10)));
         uint id = allVillagers.length - 1;
         // address ownerAddress = msg.sender;
         villagerToOwner[id] = msg.sender;
@@ -155,18 +156,11 @@ contract YourContract {
     
 
     // equip(_dna, _equipment) adds _equipment into a villager's inventory if inventory isn't full
-    function equip(uint _dna, NFT _equipment) public {
-        for (uint _i = 0; _i < maxInventorySpace; ++_i) {
-            if (villager.inventory[_i].id == 0) {
-                villager.inventory[_i] = _equipment;
-                break;
-            } else if (_i == maxInventorySpace - 1) {
-                // Create an error message stating inventory is full
-            }
-        }
+    function equip(uint _villagerIndex, uint _NFTIndex) public {
+        // NFT storage selectedNFT = allNFTs[_NFTIndex];
+        // Villager storage selectedVillager = allVillagers[_villagerIndex];
+        allVillagers[_villagerIndex].inventory.push(_NFTIndex);
     }
-
-
 
 
 
