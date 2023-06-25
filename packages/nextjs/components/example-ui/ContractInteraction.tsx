254aba0572dd0191470dd3b18f150121e6da1420
YLinDev <adikit1@icloud.com>
2023-06-24 21:38:03 -0400

    attributes

--- packages/nextjs/components/example-ui/ContractInteraction.tsx
+++ packages/nextjs/components/example-ui/ContractInteraction.tsx
@@ -47,14 +47,23 @@ export const ContractInteraction = () => {
     <div className="flex bg-base-300 relative pb-10">
       <div className="flex flex-col w-full mx-5 sm:mx-8 2xl:mx-20">
         <div className="flex flex-col mt-6 px-7 py-8 bg-base-200 opacity-80 rounded-2xl shadow-lg border-2 border-primary">
-          <span className="text-4xl sm:text-6xl text-black">Welcome to FounderVille</span>
+          <span className="text-4xl sm:text-6xl text-#004D00">Welcome to FounderVille</span>
           <br/>
           <div>
             {villagerTitle()}
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col self-center">
             {Object.values(yourVillagers).map((villager, idx) => (
-              <a href={`https://tokenbound.org/assets/polygon/${contractAddress}/${idx}`} target="_blank" className="bg-white transition-colors duration-300 ease-in-out hover:bg-black">{villager.name}</a>
+              <a href={`https://tokenbound.org/assets/polygon/${contractAddress}/${idx}`} target="_blank" className="bg-white transition-colors duration-300 ease-in-out hover:bg-green-900">
+                Name: {villager.name}
+                <br/>
+                Dexterity: {parseInt(villager.dex._hex, 16)}
+                <br/>
+                Intelligence: {parseInt(villager.intelligence._hex, 16)}
+                <br/>
+                Strength: {parseInt(villager.strength._hex, 16)}
+                <img className="rounded-lg" src={`https://noun.pics/${parseInt(villager.dna._hex, 16)}`}></img>
+              </a>
             ))}
           </div>
         </div>
